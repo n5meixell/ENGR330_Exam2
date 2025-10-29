2cebeb339eee8a959e193c738659d760b10896e1
Meixell <meix5579@STTHOMAS.EDU>
2025-10-28 20:06:34 -0500

    Adjustment(Prefix) When adding commits I accidently deleleted some code which made it invalid.

--- adder_rtl/prefix_adder.v
+++ adder_rtl/prefix_adder.v
@@ -9,9 +9,9 @@
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: Parameterized prefix adder (Kogge-Stone style example)
 // 
-// Dependencies: 
+// Dependencies: None
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -19,49 +19,44 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module prefix_adder #(parameter N = 8) (
-input wire [N-1:0] x,
-input wire [N-1:0] y,
-input wire Cin,
-output reg [N-1:0] s,
-output reg Cout
-    );
-    reg [N-1:0] p, g;
-    reg [N:0] c;
-    reg [N-1:0] g_stage [0:6];
-    reg [N-1:0] p_stage [0:6];
-    
-    integer i, j , k;
-    
+    input wire [N-1:0] x,
+    input wire [N-1:0] y,
+    input wire Cin,
+    output reg [N-1:0] s,
+    output reg Cout
+);
+ reg [N-1:0] p, g;
+reg [N:0] c;
+reg [N-1:0] gen [0:6];
+reg [N-1:0] prop [0:6];
+integer i, j, k;
 
-    always @(*) begin
-    for (i = 0; i < N; i = i + 1)begin
-    p_stage[0][i] = x[i] | y[i];
-    g_stage[0][i] = x[i] & y[i];
-    end
-    
-    for (i =1; i <= 6; i = i +1) begin
-    for (j = 0; j < N; j = j + 1) begin
-    if (j >= (1 << (i-1))) begin
-    p_stage[i][j] = p_stage[i-1][j] & p_stage[i-1][j - (1 << (i-1))];
-    g_stage[i][j] = g_stage[i-1][j] | (p_stage[i-1][j] & g_stage[i-1][j - (1 << (i-1))]);
-    end else begin
-    p_stage[i][j] = p_stage[i-1][j];
-    g_stage[i][j] = p_stage[i-1][j];
-    end
-    end
-    end
-        c[0] = Cin;
-    for (k =0; k < N; k = k + 1) begin
-    c[k+1] = g_stage[6][k] | (p_stage[6][k] & Cin);
-    end
-    
-    for (i = 0; i < N; i = i + 1) begin
-    s[i] = x[i] ^ y[i] ^ c[i];
-    end 
-    Cout = c[N];
-    end
-    
-    
+always @(*) begin
+for (i = 0; i < N; i = i + 1) begin
+prop[0][i] = x[i] | y[i];
+gen[0][i]  = x[i] & y[i];
+ end
+for (i = 1; i <= 6; i = i + 1) begin
+for (j = 0; j < N; j = j + 1) begin
+ if (j >= (1 << (i - 1))) begin
+  prop[i][j] = prop[i-1][j] & prop[i-1][j - (1 << (i - 1))];
+gen[i][j]  = gen[i-1][j] | (prop[i-1][j] & gen[i-1][j - (1 << (i - 1))]);
+ end else begin
+ prop[i][j] = prop[i-1][j];
+gen[i][j]  = gen[i-1][j];
+end
+end
+end
+
+c[0] = Cin;
+for (k = 0; k < N; k = k + 1) begin
+ c[k + 1] = gen[6][k] | (prop[6][k] & Cin);
+end
+
+ for (i = 0; i < N; i = i + 1) begin
+ s[i] = x[i] ^ y[i] ^ c[i];
+end
+ Cout = c[N];
+end
 endmodule
